74f31183f0867e3c4001dddda1bd7a88e6f2f3f5
Pierre <33554755+PierreIsep@users.noreply.github.com>
2018-05-07 09:47:20 +0200

    Update connexion.php

--- Modèle/connexion.php
+++ Modèle/connexion.php
@@ -2,16 +2,13 @@
 
 try{
 <<<<<<< HEAD
-    $bdd = new PDO('mysql:host=localhost;dbname=BaseDonn�e;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=BaseDonnée;charset=utf8', 'root', '');
 =======
     $bdd = new PDO('mysql:host=localhost;dbname=BaseDonnée;charset=utf8', 'root', '');
->>>>>>> e95221836424737d164622ff663f195f8a77b93e
 }
 catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
-<<<<<<< HEAD
+
 }
 =======
-}
->>>>>>> e95221836424737d164622ff663f195f8a77b93e
