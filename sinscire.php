4222fd209b0fbd7f326eed4d1c48fb538e759a0c
nerotej <37829930+nerotej@users.noreply.github.com>
2018-05-13 18:58:26 +0200

    Add files via upload

--- sinscire.php
+++ sinscire.php
@@ -1,28 +1,30 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=basedonnee;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=users;charset=utf8', 'root', 'root');
 	array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 }
 catch (Exception $e)
 {
 	die('Erreur : ' . $e->getMessage());
 }
-
-$pseudo= htmlspecialchar($_POST['pseudo']);
-$email=htmlspecialchar($_POST['email']);
-$age=htmlspecialchar($_POST['age']);
+if (!empty($_POST['pseudo']) AND !empty($_POST['nom'])  AND !empty($_POST['age']) AND !empty($_POST['mdp']))
+{
+	//*fonction de vérification des éléments en java //
+$pseudo=($_POST['pseudo']);
+$nom=($_POST['nom']);
+$age=($_POST['age']);
 $mdp=sha1($_POST['mdp']);
 
+$donnees= $bdd->prepare('INSERT INTO utilisateurs (pseudo, mdp, age, nom) VALUES (?, ?, ?, ? )');
 
-$donnees= $bdd->prepare('INSERT INTO users (pseudo, mdp, age, nom) VALUES (?, ?, ?, ? )');
-
-$donnees->execute(array(
-		 $pseudo,
-		 $email,
-		$age,
-		$mdp
-		));
+$donnees->execute(array($pseudo,$mdp,$age,$nom));
 
 include('tableaudebord.php');
+}
+else
+{
+echo " Remplissez tous les champs svp !" ; 
+include ('formulaireinscription.php') ; 
+}
 ?>
