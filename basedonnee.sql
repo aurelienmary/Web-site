5185d6964d739b2018a3b844c902fb23af33a982
Pierre <33554755+PierreIsep@users.noreply.github.com>
2018-05-14 01:10:29 +0200

    mvc

--- basedonnee.sql
+++ basedonnee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 10 mai 2018 à 13:02
+-- Généré le :  Dim 13 mai 2018 à 23:09
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -24,23 +24,205 @@ SET time_zone = "+00:00";
 
 -- --------------------------------------------------------
 
+--
+-- Structure de la table `building`
+--
 
-/*
- * Structure de la table `users`
-*/
+DROP TABLE IF EXISTS `building`;
+CREATE TABLE IF NOT EXISTS `building` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `consomax` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `catalog`
+--
+
+DROP TABLE IF EXISTS `catalog`;
+CREATE TABLE IF NOT EXISTS `catalog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cgu`
+--
+
+DROP TABLE IF EXISTS `cgu`;
+CREATE TABLE IF NOT EXISTS `cgu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `function`
+--
+
+DROP TABLE IF EXISTS `function`;
+CREATE TABLE IF NOT EXISTS `function` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `state` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `home`
+--
+
+DROP TABLE IF EXISTS `home`;
+CREATE TABLE IF NOT EXISTS `home` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `adress` varchar(255) NOT NULL,
+  `building_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `building_id` (`building_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mentionlegale`
+--
+
+DROP TABLE IF EXISTS `mentionlegale`;
+CREATE TABLE IF NOT EXISTS `mentionlegale` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panne`
+--
+
+DROP TABLE IF EXISTS `panne`;
+CREATE TABLE IF NOT EXISTS `panne` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `date` date DEFAULT NULL,
+  `state` tinyint(1) DEFAULT '0',
+  `users_id` int(11) NOT NULL,
+  `pannetype_id` int(11) NOT NULL,
+  `sensor_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_id` (`users_id`),
+  KEY `pannetype_id` (`pannetype_id`),
+  KEY `sensor_id` (`sensor_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pannetype`
+--
+
+DROP TABLE IF EXISTS `pannetype`;
+CREATE TABLE IF NOT EXISTS `pannetype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `piece`
+--
+
+DROP TABLE IF EXISTS `piece`;
+CREATE TABLE IF NOT EXISTS `piece` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `home_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `home_id` (`home_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `repair`
+--
+
+DROP TABLE IF EXISTS `repair`;
+CREATE TABLE IF NOT EXISTS `repair` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `repairdate` date DEFAULT NULL,
+  `cost` int(11) DEFAULT NULL,
+  `panne_id` int(11) NOT NULL,
+  `technician_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `panne_id` (`panne_id`),
+  KEY `technician_id` (`technician_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sensors`
+--
 
 DROP TABLE IF EXISTS `sensors`;
 CREATE TABLE IF NOT EXISTS `sensors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `valeur` int(11) NOT NULL,
-  FOREIGN KEY(`sensortype_id`) REFERENCES  `sensorstype`(`id`),
-  FOREIGN KEY(`piece_id`) REFERENCES `piece`(`id`),
-  FOREIGN KEY(`function_id`) REFERENCES `function`(`id`),
-  FOREIGN KEY(`users_id`) REFERENCES `users`(`id`),
-  FOREIGN KEY(`catalog_id`) REFERENCES `catalog`(`id`),
-  PRIMARY KEY (`id`)
+  `sensortype_id` int(11) NOT NULL,
+  `piece_id` int(11) NOT NULL,
+  `function_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  `catalog_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sensortype_id` (`sensortype_id`),
+  KEY `piece_id` (`piece_id`),
+  KEY `function_id` (`function_id`),
+  KEY `users_id` (`users_id`),
+  KEY `catalog_id` (`catalog_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sensorstype`
+--
+
+DROP TABLE IF EXISTS `sensorstype`;
+CREATE TABLE IF NOT EXISTS `sensorstype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `technician`
+--
+
+DROP TABLE IF EXISTS `technician`;
+CREATE TABLE IF NOT EXISTS `technician` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phonenumber` int(11) DEFAULT NULL,
+  `society` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -51,7 +233,7 @@ CREATE TABLE IF NOT EXISTS `sensors` (
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
@@ -61,185 +243,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `adress` varchar(255) NOT NULL,
   `postalcode` int(5) NOT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`) 
+  PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
-
-
--- Structure de la table 'sensors'
 --
-DROP TABLE IF EXISTS `sensorstype`;
-CREATE TABLE `sensorstype` ( 
--- Index pour la table `SensorsType`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------ 
-	`name` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-------------------
-
--- Structure de la table 'Catalog'
---
-DROP TABLE IF EXISTS `catalog`;
-CREATE TABLE `catalog` ( 
--- Index pour la table `Catalog`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------ 	
-	`name` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-	
----------------
-
--- Structure de la table 'Function'
---
-DROP TABLE IF EXISTS `function`;
-CREATE TABLE `function` (
--- Index pour la table `Function`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------ 
-	`name` varchar(255) NOT NULL,
-	vstate` BOOLEAN DEFAULT TRUE,
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-	
---------------
-
--- Structure de la table 'Building'
---
-DROP TABLE IF EXISTS `building`;
-CREATE TABLE `building` (
--- Index pour la table `Building`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------
-	`name` varchar(255) NOT NULL,
-	`consomax` INTEGER NOT NULL,
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-	
--------------
-
--- Structure de la table 'Home'
---
-DROP TABLE IF EXISTS `home`;
-CREATE TABLE `home`(
--- Index pour la table `Building`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
-----
-	`adress` varchar(255) NOT NULL,
-	FOREIGN KEY(`building_id`) REFERENCES `building`(`id`),
-	PRIMARY KEY (`id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
------------------
-
---Structure de la table Pi�ce
---
-DROP TABLE IF EXISTS `piece`;
-CREATE TABLE `piece`( 
--- Index pour la table `Pi�ce`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------
-	`name` varchar(255) NOT NULL,
-	`home` REFERENCES home(id,address),
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--------------
-
---Structure de la table Panne
---
-DROP TABLE IF EXISTS `panne`;
-CREATE TABLE `panne` (
--- Index pour la table `Panne`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------
-	`name` varchar(255) NOT NULL,
-	`date` DATE,
-	`state` BOOLEAN DEFAULT FALSE
-	FOREIGN KEY(`users_id`) REFERENCES `users`(`id`),
-	FOREIGN KEY(`pannetype_id`) REFERENCES `pannetype`(`id`),
-	FOREIGN KEY(`sensor_id`) REFERENCES `sensors`(`id`),
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-	
----------------
-
--- Structure de la table PanneType
---
-DROP TABLE IF EXISTS `pannetype`;
-CREATE TABLE `pannetype` (
--- Index pour la table `PanneType`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------
-	`name` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-	
-------------
-
--- Structure de la table Technician
---
-DROP TABLE IF EXISTS `technician`;
-CREATE TABLE `technician` (
--- Index pour la table `Technician`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------
-	`email` varchar(255) NOT NULL,
-	`lastname` varchar(255) NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`phonenumber` INTEGER,
-	`society` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-	
--------------
-
--- Structure de la table Repair
---
-DROP TABLE IF EXISTS `repair`;
-CREATE TABLE `repair` (
--- Index pour la table `Repair`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
-	PRIMARY KEY(`id`), 
------
-	`repairdate` DATE,
-	`cost` INTEGER,
-	FOREIGN KEY(`panne_id`) REFERENCES `panne`(`id`)
-	FOREIGN KEY(`technician_id`) REFERENCES `technician`(`id`),
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-	
-----------------
-
--- Structure de la table CGU
---
-DROP TABLE IF EXISTS `CGU`;
-CREATE TABLE `CGU` (
--- Index pour la table `CGU`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------
-	`text` varchar(60000),
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-----------------
-
--- Structure de la table MentionLegale
---
-DROP TABLE IF EXISTS `mentionlegale`;
-CREATE TABLE `mentionlegale` (
--- Index pour la table `MentionLegale`
-	`id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1,
------
-	`text` varchar(60000),
-	PRIMARY KEY (`id`)
-	)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-	
-	--
 -- Déchargement des données de la table `users`
 --
 
@@ -247,9 +255,46 @@ INSERT INTO `users` (`id`, `email`, `password`, `lastname`, `name`, `birthdate`,
 (1, 'john.smith@gmail.com', '1475369', 'SMITH', 'John', '1992-05-22', 645856951, '28 rue albert dupond', 75015, 0),
 (2, 'pierre.numa@isep.fr', '1475369', 'NUMA', 'Pierre', '1997-03-22', 1025648595, '24 rue de la victoire', 75006, 1);
 
-	
-	
-------------------
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `home`
+--
+ALTER TABLE `home`
+  ADD CONSTRAINT `home_ibfk_1` FOREIGN KEY (`building_id`) REFERENCES `building` (`id`);
+
+--
+-- Contraintes pour la table `panne`
+--
+ALTER TABLE `panne`
+  ADD CONSTRAINT `panne_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `panne_ibfk_2` FOREIGN KEY (`pannetype_id`) REFERENCES `pannetype` (`id`),
+  ADD CONSTRAINT `panne_ibfk_3` FOREIGN KEY (`sensor_id`) REFERENCES `sensors` (`id`);
+
+--
+-- Contraintes pour la table `piece`
+--
+ALTER TABLE `piece`
+  ADD CONSTRAINT `piece_ibfk_1` FOREIGN KEY (`home_id`) REFERENCES `home` (`id`);
+
+--
+-- Contraintes pour la table `repair`
+--
+ALTER TABLE `repair`
+  ADD CONSTRAINT `repair_ibfk_1` FOREIGN KEY (`panne_id`) REFERENCES `panne` (`id`),
+  ADD CONSTRAINT `repair_ibfk_2` FOREIGN KEY (`technician_id`) REFERENCES `technician` (`id`);
+
+--
+-- Contraintes pour la table `sensors`
+--
+ALTER TABLE `sensors`
+  ADD CONSTRAINT `sensors_ibfk_1` FOREIGN KEY (`sensortype_id`) REFERENCES `sensorstype` (`id`),
+  ADD CONSTRAINT `sensors_ibfk_2` FOREIGN KEY (`piece_id`) REFERENCES `piece` (`id`),
+  ADD CONSTRAINT `sensors_ibfk_3` FOREIGN KEY (`function_id`) REFERENCES `function` (`id`),
+  ADD CONSTRAINT `sensors_ibfk_4` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `sensors_ibfk_5` FOREIGN KEY (`catalog_id`) REFERENCES `catalog` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
