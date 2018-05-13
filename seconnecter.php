f384703a4850a37d6b8428d7f4b95c695fcaf1d6
nerotej <37829930+nerotej@users.noreply.github.com>
2018-05-13 18:59:00 +0200

    Add files via upload

--- seconnecter.php
+++ seconnecter.php
@@ -11,14 +11,14 @@ catch (Exception $e)
 	die('Erreur : ' . $e->getMessage());
 }
 $midentconnect=($_POST['pseudo']);
-$email=($_POST['email']);
+$mdp=($_POST['mdp']);
 
 
-if(!empty($midentconnect) AND !empty($email))
+if(!empty($midentconnect) AND !empty($mdp))
 {
 	$requete = $bdd->prepare('SELECT * FROM utilisateurs WHERE pseudo=? AND mdp=?');
 	
-	$requete->execute(array($midentconnect, $email));
+	$requete->execute(array($midentconnect, $mdp));
 	$userexist= $requete->rowCount ();
 	if ($userexist==1)
 	{
