a8e35388f089ac44db9a6767c14f8481cdc54200
aurélien <aurelienmary.fr@gmail.com>
2018-05-15 12:13:48 +0200

    include

--- Vue/connexion.php
+++ Vue/connexion.php
@@ -4,15 +4,15 @@
         <meta charset="utf-8">
         <title>Page d'accueil</title>
         <link href="styles1.css" rel="stylesheet">
-         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
         <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet"> 
         
     </head>
     
     
     <body>
-        
-        <header>
+        <?php include("header.php"); ?>
+        <!--  <header>
 		     <div class="header-box">
 		     	<div class="logo">
 		            <img id="img" src="images/notre-logo.png" alt="notre logo" />
@@ -43,7 +43,7 @@
 		                </nav>            
 		     </div>           
             
-     </header>
+     </header>-->
      
      <section> 
     
--- Vue/header.php
+++ Vue/header.php
@@ -1,44 +1,44 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>Page d'accueil</title>
-        <link href="styles1.css" rel="stylesheet">
+        <link href="header.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet"> 
         
     </head>
     
     
-    <body>
-        
- <header>
-     <div class="header-box">
-         <div class="logo">
-            <img id="img" src="images/notre-logo.png" alt="notre logo" />
-        </div>
-         <div class="search-bar"> 
-             <form class="searchform cf">
-                  <input type="text" placeholder="Rechercher">
-                  <button type="submit">Search</button>
-             </form>
-         </div>
-         <div class="reglog">
-             <p><img src="images/man-user.png" alt='avatar'/><a href="#"> Connexion </a></p>
-         </div>
-     </div>
-     <div class="wrapper-bis"> 
-         
-                <nav>
-                    <ul>
-                        <li><a href="index.html">Accueil</a></li>
-                        <li><a href="#">A propos</a></li>
-                        <li><a href="Guides">Produits</a></li>
-                        <li><a href="Contact">Contact</a></li>
-                    </ul>
-                
-                </nav>            
-     </div>           
-            
-        </header>
-	
-      </body>
+   		<header>
+		     <div class="header-box">
+		     	<div class="logo">
+		            <img id="img" src="images/notre-logo.png" alt="notre logo" />
+		        </div>
+		        	<div class="search-bar"> 
+		            	
+		              
+		              		<h1>DOMISEP</h1>
+		         
+		         
+		        		
+		        	</div>
+		        <div class="reglog">
+		             <p><img src="images/man-user.png" alt='avatar'/><a href="#"> Connexion </a></p>
+		        </div>
+		     </div>
+		     
+		     <div class="wrapper-bis"> 
+		         
+		                <nav>
+		                    <ul>
+		                        <li><a href="index.html">Accueil</a></li>
+		                        <li><a href="#">A propos</a></li>
+		                        <li><a href="Guides">Produits</a></li>
+		                        <li><a href="Contact">Contact</a></li>
+		                    </ul>
+		                
+		                </nav>            
+		     </div>           
+		</header>
+     
 </html>
