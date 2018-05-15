324cbfc731087bdb6f624f8e7241e67752a4b1f1
aurélien <aurelienmary.fr@gmail.com>
2018-05-15 11:49:13 +0200

    footer include

--- Vue/connexion.php
+++ Vue/connexion.php
@@ -81,40 +81,8 @@
 
      </section>  
 
-
-        <footer> 
-            
-            <div class="footer-box">
-                <div class="resauxSociaux">
-                    <h3>Suivez-nous: </h3>
-                     <a href="#"><img src='images/facebook-logo.png'/></a> 
-                     <a href="#"><img src='images/instagram.png'/></a>
-                     <a href="#"><img src='images/twitter.png'/></a>
-                     <p>28, rue Notre-Dame-des-Champs<br>75006 Paris</p>
-                     
-                </div>
-                <div class="renseignements"> 
-                    <img src='images/question.png' alt='img-question' />
-                    <h3>AIDE & CONTACT</h3>
-                    <p>Une question? Un renseignement? <br> 
-                    <p>Contacter-nous</p>
-                    <div class="ml">
-                    	<p><a href="#">Mentions légales</a></p>
-                	</div>
-                </div>
-                <div class="catalogues">  
-                    <h3 class="gc"> <a href="index4"> Les catalogues & Guides </a></h3>
-                    <p>Retrouver tous les catalogues et <br> guides pour réaliser tous vos projets</p>
-                </div>
-               
-            </div>
-            
-            
-            
-             
-           
-           
-        </footer>
+		<?php include("footer.php"); ?>
+       	
         
         
         
--- Vue/footer.php
+++ Vue/footer.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+		<link href="footer.css" rel="stylesheet">
+		
+	</head>
 
         <footer> 
             
@@ -7,22 +13,26 @@
                      <a href="#"><img src='images/facebook-logo.png'/></a> 
                      <a href="#"><img src='images/instagram.png'/></a>
                      <a href="#"><img src='images/twitter.png'/></a>
+                     <p>28, rue Notre-Dame-des-Champs<br>75006 Paris</p>
+                     
                 </div>
                 <div class="renseignements"> 
                     <img src='images/question.png' alt='img-question' />
                     <h3>AIDE & CONTACT</h3>
-                    <p>Une question? Un renseignement? <br> <p>Contacter-nous</p>
+                    <p>Une question? Un renseignement? <br> 
+                    <p>Contacter-nous</p>
+                    <div class="ml">
+                    	<p><a href="#">Mentions légales</a></p>
+                	</div>
                 </div>
                 <div class="catalogues">  
-                    <h3 class="gc"> <a href="#"> Les catalogues & Guides </a></h3>
+                    <h3 class="gc"> <a href="index4"> Les catalogues & Guides </a></h3>
                     <p>Retrouver tous les catalogues et <br> guides pour réaliser tous vos projets</p>
                 </div>
                
             </div>
-             <div class="ml">
-                    Mentions légales
-                </div>
-           
-           
+            
+         
         </footer>
         
+</html>
