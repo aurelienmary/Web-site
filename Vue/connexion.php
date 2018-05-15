e9f5dc944ab6e7180aa1c4080ea10fa9869c6cd9
aurélien <aurelienmary.fr@gmail.com>
2018-05-15 12:24:01 +0200

    html footer

--- Vue/connexion.php
+++ Vue/connexion.php
@@ -12,38 +12,7 @@
     
     <body>
         <?php include("header.php"); ?>
-        <!--  <header>
-		     <div class="header-box">
-		     	<div class="logo">
-		            <img id="img" src="images/notre-logo.png" alt="notre logo" />
-		        </div>
-		        	<div class="search-bar"> 
-		            	<div class="search-bar">
-		              
-		              <h1>DOMISEP</h1>
-		         
-		         
-		        		</div>
-		        	</div>
-		        <div class="reglog">
-		             <p><img src="images/man-user.png" alt='avatar'/><a href="#"> Connexion </a></p>
-		        </div>
-		     </div>
-		     
-		     <div class="wrapper-bis"> 
-		         
-		                <nav>
-		                    <ul>
-		                        <li><a href="index.html">Accueil</a></li>
-		                        <li><a href="#">A propos</a></li>
-		                        <li><a href="Guides">Produits</a></li>
-		                        <li><a href="Contact">Contact</a></li>
-		                    </ul>
-		                
-		                </nav>            
-		     </div>           
-            
-     </header>-->
+        
      
      <section> 
     
