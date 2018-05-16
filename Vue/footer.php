8dddc2f2b4c15f44d6609497b55bdf6419f8fd40
aurélien <aurelienmary.fr@gmail.com>
2018-05-16 11:34:35 +0200

    css

--- Vue/footer.php
+++ Vue/footer.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
     	<meta charset="utf-8">
-		<link href="../public/css/footer.css" rel="stylesheet">
+		<link href="./public/css/footer.css" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
         <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet"> 
 		
