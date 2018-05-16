7afbac4d47849bae82717c1c7affba0152ca3bde
Pierre <33554755+PierreIsep@users.noreply.github.com>
2018-05-16 10:43:54 +0200

    base donnée avec info enregistré dedans

--- basedonnee.sql
+++ basedonnee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 16 mai 2018 à 08:42
+-- Généré le :  mer. 16 mai 2018 à 08:43
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
