99ec5e092a78621b80af1dd9887cb756461d6b49
ouni10854 <37830273+ouni10854@users.noreply.github.com>
2018-05-22 09:47:21 +0200

    Update catalogue.php

--- Vue/catalogue.php
+++ Vue/catalogue.php
@@ -5,7 +5,7 @@
         <title>Page d'accueil</title>
         <link href="public/css/catalogue.css" rel="stylesheet">
         
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
