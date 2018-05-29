5249e8c3968e04dfe1bfde88f18879954a8e16a4
ouni10854 <37830273+ouni10854@users.noreply.github.com>
2018-05-29 09:53:18 +0200

    Add files via upload

---
+++ public/BDpageCapteur.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Dim 27 Mai 2018 à 20:34
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `interface_siteweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pieces`
+--
+
+CREATE TABLE IF NOT EXISTS `pieces` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `pieces`
+--
+
+INSERT INTO `pieces` (`id`, `name`) VALUES
+(1, 'Menus'),
+(2, 'Burgers'),
+(3, 'Snacks'),
+(4, 'Salades'),
+(5, 'Boissons'),
+(6, 'Desserts');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `capteurs`
+--
+
+CREATE TABLE IF NOT EXISTS `capteurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom du capteur` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `logement` float NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `piece` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `piece` (`piece`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+
+--
+-- Contenu de la table `capteurs`
+--
+
+INSERT INTO `capteurs` (`id`, `nom du capteur`, `description`, `logement`, `image`, `piece`) VALUES
+(17, 'capteur de temperature', '...', 1, 's5.png', 'chambre 1'),
+(33, 'capteur de mouvement', '...', 2, 'd5.png', 'salon');
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `capteur`
+--
+ALTER TABLE `capteurs`
+  ADD CONSTRAINT `capteurs_ibfk_1` FOREIGN KEY (`piece`) REFERENCES `pieces` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
