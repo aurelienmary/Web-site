67e0a78c631ba400484572d91c9b3c8f81c76042
Pierre <33554755+PierreIsep@users.noreply.github.com>
2018-06-04 11:04:23 +0200

    piece

--- Modele/requetes.capteurs.php
+++ Modele/requetes.capteurs.php
@@ -13,7 +13,7 @@ function getusersensor(PDO $bdd, array $user)
 	curl_setopt(
 			$ch,
 			CURLOPT_URL,
-			"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=009E");
+			"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=009A");
 	curl_setopt($ch, CURLOPT_HEADER, FALSE);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 	$data = curl_exec($ch);
--- Modele/requetes.generiques.php
+++ Modele/requetes.generiques.php
@@ -9,3 +9,9 @@ function getall (PDO $bdd, string $table)
     $query = 'SELECT * FROM'.$table;
     return $bdd->query($query)->fetchAll();
 }
+
+function searchfrom (PDO $bdd, string $table, string $line)
+{
+    $query = 'SELECT '.$line.' FROM'.$table;
+    return $bdd->query($query)->fetchAll();
+}
