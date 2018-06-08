17dc40dea836a02bc6fb7b9db5dae52a8c4f1148
Pierre <33554755+PierreIsep@users.noreply.github.com>
2018-06-08 10:10:53 +0200

    afichage bienvenue utilisateur

--- Vue/header.php
+++ Vue/header.php
@@ -32,7 +32,7 @@
 			    		
 			    		
 			    		<p><img src="public/images/man-user.png" alt='avatar'/><a href="index.php?cible=utilisateurs&fonction=deconnexion"> Deconnexion </a></p>
-			    		<p> <?php echo $_SESSION['name'] . '  ' . $_SESSION['lastname']?> </p>
+			    		<p> Bienvenue <?php echo $_SESSION['name'] . '  ' . $_SESSION['lastname']?> </p>
 			    		
 			    		</div>
     		
