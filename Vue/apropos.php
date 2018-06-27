a0f8e05c6c7b00d07ab277a728cb38f909d35ce6
PierreNICOLAI1 <37829948+PierreNICOLAI1@users.noreply.github.com>
2018-06-27 20:08:55 +0200

    Create apropos.php

---
+++ Vue/apropos.php
@@ -0,0 +1,16 @@
+
+
+
+
+
+Well-Home est le nom que nous avons choisi pour notre entreprise.
+Ce nom fait référence au mot anglais « Welcome » qui signifie bienvenue.
+Nous avons décidé de le décomposer en deux mots afin de créer un contact entre les deux.
+
+Tout d’abord, « Well » fait référence à tout ce qui est bien fait, bien conçu. Nous avons souhaité insister sur la satisfaction de l’utilisateur, que notre projet soit en harmonie avec ses attentes.
+Ensuite, « Home » désigne le foyer évoquant l’idée du chez soi pour se sentir « à la maison ».
+
+Well-Home, Bienvenue chez vous !
+
+Afin d’illustrer notre nom, nous avons fait un logo assez représentatif où nous pouvons facilement deviner une maison, avec en son centre le sigle du bouton POWER rappelant le contrôle de la maison de manière simple, et efficace à l’aide de la technologie.
+L’omniprésence de la couleur verte renvoie à l’importance d’une habitation propre, qui respecte les normes écologiques et qui utilise au maximum les ressources à sa disposition.
