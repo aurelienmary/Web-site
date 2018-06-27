e58450230a096a57eae32c69bc45263fff3c9f7e
ouni10854 <37830273+ouni10854@users.noreply.github.com>
2018-06-27 12:34:50 +0200

    Update ajoutcapteur.php

--- Vue/ajoutcapteur.php
+++ Vue/ajoutcapteur.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title>Page d'accueil</title>
          
-        <link href="./public/css/ajoutcapteur.css" rel="stylesheet">
+        <link href="styles7.css" rel="stylesheet">
          <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
         <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet"> 
         
@@ -13,8 +13,70 @@
     
     <body>
         
- 
-       
+ <header>
+     <div class="header-box">
+         <div class="logo">
+            <img id="img" src="images/notre-logo.png" alt="notre logo" />
+        </div>
+         <div class="search-bar"> 
+              <div class="search-bar">
+              
+              <h1>DOMISEP</h1>
+         
+         
+         </div>
+         </div>
+         <div class="reglog">
+             <p><img src="images/man-user.png" alt='avatar'/><a href="#"> Déconnexion </a></p>
+         </div>
+     </div>
+     <div class="wrapper-bis"> 
+         
+                <nav>
+                    <ul>
+                        <li><a href="index.html">Accueil</a></li>
+                        <li><a href="#">A propos</a></li>
+                        <li><a href="Guides">Produits</a></li>
+                        <li><a href="Contact">Contact</a></li>
+                    </ul>
+                
+                </nav>            
+     </div>           
+            
+        </header>
+        
+        
+        
+        
+        
+        
+        
+        
+        <span class="bckg"></span>
+        <div class="dashboard">
+  <h1>Tableau de bord</h1>
+        
+  <nav>
+    <ul>
+      <li>
+        <a href="admin/index.php" data-title="Projects">Gestion capteurs</a>
+      </li>
+      
+      <li>
+        <a href="javascript:void(0);" data-title="message">Messagerie</a>
+      </li>
+      <li>
+        <a href="addPiece/piece/index.php" data-title="Timeline">Gestion des pièces</a>
+      </li>
+      <li>
+        <a href="javascript:void(0);" data-title="Settings">Reglages</a>
+      </li>
+      <li>
+        <a href="javascript:void(0);" data-title="alert">Notifications</a>
+      </li>
+    </ul>
+  </nav>
+            </div>
 
 <main>
   <div class="title">
@@ -44,7 +106,7 @@
 			<div class='column-right'></div>
 		</a>
 		<a href='#' class='row'>
-			<div class='column-left'>luminosité</div>
+			<div class='column-left'>lumunosité</div>
 			<div class='column-right'></div>
 		</a>
 		<a href='#' class='row'>
@@ -111,6 +173,39 @@
         
         
         
+        
+         <footer> 
+            
+            <div class="footer-box">
+                <div class="resauxSociaux">
+                    <h3>Suivez-nous: </h3>
+                     <a href="#"><img src='images/facebook-logo.png'/></a> 
+                     <a href="#"><img src='images/instagram.png'/></a>
+                     <a href="#"><img src='images/twitter.png'/></a>
+                </div>
+                <div class="renseignements"> 
+                    <img src='images/question.png' alt='img-question' />
+                    <h3>AIDE & CONTACT</h3>
+                    <p>Une question? Un renseignement? <br> <p>Contacter-nous</p>
+                </div>
+                <div class="catalogues">  
+                    <h3 class="gc"> <a href="#"> Les catalogues & Guides </a></h3>
+                    <p>Retrouver tous les catalogues et <br> guides pour réaliser tous vos projets</p>
+                </div>
+               
+            </div>
+            
+            <div class="adress">
+                28, rue Notre-Dame-des-Champs<br>75006 Paris
+            
+            </div>
+            
+             <div class="ml">
+                    Mentions légales
+                </div>
+           
+           
+        </footer>
         
         
         
