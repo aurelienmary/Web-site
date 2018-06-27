c0533dd624755cf20cc5fe7daf2357a6f8eabcfa
nerotej <37829930+nerotej@users.noreply.github.com>
2018-06-27 21:04:49 +0200

    Update faq.php

--- Vue/faq.php
+++ Vue/faq.php
@@ -16,42 +16,43 @@
 <p> 
 Comment signaler une panne ou un problème ? 
 
-    >> Pour signaler un problème, vous pouvez contacter le service après vente qui se fera un grand plaisir de répondre à vos questions éventuelles
+    <br></br> Pour signaler un problème, vous pouvez contacter le service après vente qui se fera un grand plaisir de répondre à vos questions éventuelles <br></br>
 
 
 Comment ajouter un logement ?
 
-    >> Pour ajouter ajouter un logement, vous pouvez facilement vous rendre sur la page "gestion des pièces". Il vous faut être connecté pour pouvoir effectué cette manipulation.
+     <br></br> Pour ajouter ajouter un logement, vous pouvez facilement vous rendre sur la page "gestion des pièces". Il vous faut être connecté pour pouvoir effectué cette manipulation. </br>
+
 
 
 Comment définir les éléments d’un logement ?
 
-    >> Si vous souhaitez ajouter une pièce dans votre logement, vous devez vous rendre sur la page "gestion des pièces" où vous avez la possibilité de modifier, ajouter ou supprimer des pièces. Vous pouvez ensuite ajouter des capteurs dans chaque pièce que vous pouvez consulter notre catalogue.
+<br></br>Si vous souhaitez ajouter une pièce dans votre logement, vous devez vous rendre sur la page "gestion des pièces" où vous avez la possibilité de modifier, ajouter ou supprimer des pièces. Vous pouvez ensuite ajouter des capteurs dans chaque pièce que vous pouvez consulter notre catalogue. <br></br>
 
 
 
 Comment définir les capteurs d’une pièce ?
 
-    >> Pour définir les capteurs d'une pièce, vous devez vous rendre sur la page "gestion des capteurs". Vous pouvez alors selectionner des capteurs et les ajouter à votre liste en fonction des pièces que vous désirer.
+ <br></br>Pour définir les capteurs d'une pièce, vous devez vous rendre sur la page "gestion des capteurs". Vous pouvez alors selectionner des capteurs et les ajouter à votre liste en fonction des pièces que vous désirer.<br></br>
 
 
 
 Quels sont les droits que possèdent l’administrateur ?
 
-    >> L'administrateur a la possibilité d'ajouter, de supprimer ou de modifier les données d'un utilisateur. Il a également accès aux données des trames qui sont transmise au serveur par le Cemac. 
+ <br></br>L'administrateur a la possibilité d'ajouter, de supprimer ou de modifier les données d'un utilisateur. Il a également accès aux données des trames qui sont transmise au serveur par le Cemac. <br></br>
 
 
 
 Comment puis-je commander mes capteurs ?
 
-    >> Pour commander les capteurs, vous devez tout d'abord être connecté puis vous rendre sur la page "gestion des capteurs". Vous avez ensuite la liste des capteurs avec la possibilité de gérer leurs allumage.
+    <br></br> Pour commander les capteurs, vous devez tout d'abord être connecté puis vous rendre sur la page "gestion des capteurs". Vous avez ensuite la liste des capteurs avec la possibilité de gérer leurs allumage. <br></br>
     Si vous avez des problèmes au niveau de l'allumage des capteurs, vous pouvez commencer par vérifer qu'ils sont bien reliés à une alimentation adaptée. Si vous n'arrivez toujours pas à les faire fonctionner, vous pouvez contacter notre service après vente qui pourra vous aider.
 
 
 
 Quelles informations personnelles sont conservées dans les bases de données de Domisep ?
 
-    >> Les informations personnelles qui sont enregistrées dans la base de données Domisep sont le nom, le prénom, l'adresse mail, la date de naissance, l'adresse postale et le numéro de téléphone de tous les utilisateurs. Domisep a également accès à toutes les données relative aux capteurs et à leurs fonctionnement.
+    <br></br>Les informations personnelles qui sont enregistrées dans la base de données Domisep sont le nom, le prénom, l'adresse mail, la date de naissance, l'adresse postale et le numéro de téléphone de tous les utilisateurs. Domisep a également accès à toutes les données relative aux capteurs et à leurs fonctionnement.
 
 </p>
 
