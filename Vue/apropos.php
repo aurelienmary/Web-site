1f17e7abcf17c22aa6babe2f6c0625edb7c747b2
PierreNICOLAI1 <37829948+PierreNICOLAI1@users.noreply.github.com>
2018-06-27 20:10:27 +0200

    Update apropos.php

--- Vue/apropos.php
+++ Vue/apropos.php
@@ -1,7 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Page d'inscription</title>
+        <link href="public/css/inscription.css" rel="stylesheet">
+        <script type="text/javascript" language="javascript" src="script2.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
+          
+        <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet"> 
+        
+        
+</head>
 
 
-
-
+<body>
 
 Well-Home est le nom que nous avons choisi pour notre entreprise.
 Ce nom fait référence au mot anglais « Welcome » qui signifie bienvenue.
@@ -14,3 +26,7 @@ Well-Home, Bienvenue chez vous !
 
 Afin d’illustrer notre nom, nous avons fait un logo assez représentatif où nous pouvons facilement deviner une maison, avec en son centre le sigle du bouton POWER rappelant le contrôle de la maison de manière simple, et efficace à l’aide de la technologie.
 L’omniprésence de la couleur verte renvoie à l’importance d’une habitation propre, qui respecte les normes écologiques et qui utilise au maximum les ressources à sa disposition.
+
+  </body>
+  
+</html>
