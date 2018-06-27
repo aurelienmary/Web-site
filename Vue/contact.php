944f2b47de6a378d77f6d70e1e1bdffc093467a5
aurélien <aurelienmary.fr@gmail.com>
2018-06-27 18:56:53 +0200

    contact

--- Vue/contact.php
+++ Vue/contact.php
@@ -1,22 +1,32 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> 17b5744563b218f48a21c65ed003117abb7653fb
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
+<<<<<<< HEAD
         <link rel="stylesheet" href="./public/css/stat.css" />
+=======
+        <link rel="stylesheet" href="./public/css/contact.css" />
+>>>>>>> 17b5744563b218f48a21c65ed003117abb7653fb
         <title> Contact </title>
     </head>
 
 <body>
 
-<div class = "contact">
+<div class = "titre">
 <h1> Contact </h1>
+</div>
 
+<div class = "contact">
 <h2> Nous contacter </h2>
 <p>
     Vous pouvez nous contacter par téléphone au +336 69 04 36 72 </br>
     Par mail à l'adresse well-home@gmail.com </br>
     Vous pouvez également nous contacter sur les réseaux sociaux : Facebook<a href="https://www.facebook.com"><img src='public/images/facebook-logo.png'/></a>, Instagram<a href="#"><img src='public/images/instagram.png'/></a>, Twitter<a href="#"><img src='public/images/twitter.png'/></a>.                   
+
 </div>
 
 
