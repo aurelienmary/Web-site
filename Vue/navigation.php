ea1cb78615c55273fcb6cb262ba6fa79c500c39e
nerotej <37829930+nerotej@users.noreply.github.com>
2018-06-28 00:50:06 +0200

    Update navigation.php

--- Vue/navigation.php
+++ Vue/navigation.php
@@ -4,19 +4,16 @@
         <meta charset="utf-8">
         
          
-        <link href="./public/css/navigaton.css" rel="stylesheet">
-         <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet"> 
-        <link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet"> 
+        <link href="navigation.css" rel="stylesheet">
+        
         
     </head>
     
     
     <body>
-
-
-
-		<span class="bckg"></span>
-		<div class="dashboard">
+	
+	<div class="dashboard">
+	
 		  <h1>Tableau de bord</h1>
 		        
 		   <nav>
@@ -24,38 +21,28 @@
        <li>
 		 <a href="index.php?cible=utilisateurs&fonction=ajoutcapteur" data-title="Projects">Menu</a>
 	  </li>
-      <li>
+      <li></br>
         <a href="index.php?cible=utilisateurs&fonction=addSensor" data-title="Projects">Gestion capteurs</a>
       </li>
-      
+      </br>
       <li>
 		<a href="index.php?cible=utilisateurs&fonction=chat" data-title="message">Messagerie</a>
 	</li>
+	</br>
       <li>
         <a href="index.php?cible=building&fonction=piece" data-title="Timeline">Gestion des pièces</a>
       </li>
+      </br>
      <li>
 		<a href="index.php?cible=utilisateurs&fonction=profil" data-title="Settings">Profil</a>
 	 </li>
+	 </br>
 	    <li>
 		<a href="index.php?cible=utilisateurs&fonction=stat" data-title="Statistique">Statistiques</a>
 	 </li>
      
-      <li>
-        <a href="javascript:void(0);" data-title="Settings">Reglages</a>
-      </li>
-      <li>
-        <a href="javascript:void(0);" data-title="alert">Notifications</a>
-      </li>
-	 <?php 
-		      if ($_SESSION['admin'] == 1)
-		      {
-		      	?>
-		      	<li>
-		      	<a href="index.php?cible=admin&fonction=recherche" data-title="alert">Recherche</a>
-		      	</li>
-		      <?php }
-		      ?>
+	
+		   
 		    </ul>
 		  </nav>
 		            </div>
