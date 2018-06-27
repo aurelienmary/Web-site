abd5e31862c358940bf3eba291644ce4f186c4cf
Pierre <33554755+PierreIsep@users.noreply.github.com>
2018-06-27 20:12:40 +0200

    Update capteurs.php

--- Controleur/capteurs.php
+++ Controleur/capteurs.php
@@ -33,6 +33,27 @@ switch ($function)
         $vue="insert";
         $title="Ajouter un capteur";
         $alerte=false;
+                    if (isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['email']) and isset($_POST['passWord']) and isset($_POST['birthdate']) and isset($_POST['numTel']) and isset($_POST['adresse']) and isset($_POST['codepostal']))
+            {
+                
+            	$user =[
+                        'email'=> $_POST['email'],
+            	        'password'=>$_POST['passWord'],
+                        'lastname' => $_POST['nom'],
+                        'name'=> $_POST['prenom'],
+                        'birthdate'=> $_POST['birthdate'],
+                        'phonenumber'=>$_POST['numTel'],
+                        'adress'=>$_POST['adresse'],
+                        'postalcode'=>$_POST['codepostal'],
+                    
+                        ];
+                
+                addsensor($bdd,$sensor);
+                
+            }
+            $title="Inscription";
+        
+        break;
         
         
         
