0d4a6cfe29fdbd24f33818e86d6063748711577b
Antoine0910 <antoine.madrange@gmail.com>
2018-06-27 18:54:04 +0200

    Update contact.php

--- Vue/contact.php
+++ Vue/contact.php
@@ -1,22 +1,24 @@
-<?php session_start();?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
-        <link rel="stylesheet" href="stat.css" />
+        <link rel="stylesheet" href="contact.css" />
         <title> Contact </title>
     </head>
 
 <body>
 
-<div class = "contact">
+<div class = "titre">
 <h1> Contact </h1>
+</div>
 
+<div class = "contact">
 <h2> Nous contacter </h2>
 <p>
     Vous pouvez nous contacter par téléphone au +336 69 04 36 72 </br>
     Par mail à l'adresse well-home@gmail.com </br>
     Vous pouvez également nous contacter sur les réseaux sociaux : Facebook<a href="https://www.facebook.com"><img src='public/images/facebook-logo.png'/></a>, Instagram<a href="#"><img src='public/images/instagram.png'/></a>, Twitter<a href="#"><img src='public/images/twitter.png'/></a>.                   
+
 </div>
 
 
